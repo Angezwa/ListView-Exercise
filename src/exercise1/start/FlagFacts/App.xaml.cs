b6b582161a08f77b33ca40a1423c450cfa0a273b
Angezwa <56066163+Angezwa@users.noreply.github.com>
2019-11-21 16:21:25 +0200

    commit

--- src/exercise1/start/FlagFacts/App.xaml.cs
+++ src/exercise1/start/FlagFacts/App.xaml.cs
@@ -13,7 +13,8 @@ namespace FlagFacts
 
             InitializeComponent();
 
-            MainPage = new NavigationPage(new FlagDetailsPage());
+            //MainPage = new NavigationPage(new FlagDetailsPage());
+            MainPage = new NavigationPage(new AllFlagsPage());
         }
 
         protected override void OnStart()
